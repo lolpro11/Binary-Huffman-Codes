0270700f4d1fa59e7014f63aa4499fed0cacdd79
lolpro11 <lolpro11lolx@gmail.com>
2024-05-06 13:48:12 -0700

    add binary i/o

--- CompressFile.java
+++ CompressFile.java
@@ -1,3 +1,26 @@
-/**
- * Fill this file.
- */
+import java.io.*;
+
+public class CompressFile {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java CompressFile <source file> <destination file>");
+            return;
+        }
+
+        String sourceFile = args[0];
+        String destinationFile = args[1];
+
+        try (FileInputStream inputStream = new FileInputStream(sourceFile);
+             FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+    }
+}
