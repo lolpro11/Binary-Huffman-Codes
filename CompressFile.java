1016eca5fbc977ef539843ba5a68bd7385685c41
lolpro11 <lolpro11lolx@gmail.com>
2024-05-10 18:00:21 -0700

    add solutions

--- CompressFile.java
+++ CompressFile.java
@@ -1,7 +1,34 @@
 import java.io.*;
+import java.util.*;
+
+
+class HuffmanNode implements Comparable<HuffmanNode> {
+
+    char data;
+    int frequency;
+    HuffmanNode left, right;
+
+    public HuffmanNode(char data, int frequency) {
+        this.data = data;
+        this.frequency = frequency;
+        left = right = null;
+
+    }
+
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
+
+}
+
+
 
 public class CompressFile {
+
+    static Map<Character, String> huffmanCodes = new HashMap<>(); // Added this line
+
     public static void main(String[] args) {
+
         if (args.length != 2) {
             System.out.println("Usage: java CompressFile <source file> <destination file>");
             return;
@@ -11,16 +38,111 @@ public class CompressFile {
         String destinationFile = args[1];
 
         try (FileInputStream inputStream = new FileInputStream(sourceFile);
-             FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+             FileOutputStream outputStream = new FileOutputStream(destinationFile);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
 
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
+            Map<Character, Integer> frequencyMap = new HashMap<>();
+
+            int character;
+
+            while ((character = inputStream.read()) != -1) {
+                char c = (char) character;
+                frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
             }
+
+            HuffmanNode root = buildHuffmanTree(frequencyMap);
+
+            generateHuffmanCodes(root, new StringBuilder());
+
+            BitOutputStream bitOutputStream = new BitOutputStream(destinationFile);
+
+
+            encodeFile(inputStream, bitOutputStream);
+
+            ((Flushable) bitOutputStream).flush();
+
+            objectOutputStream.writeObject(huffmanCodes);
+
+            objectOutputStream.close();
+            inputStream.close();
 
         } catch (IOException e) {
             System.out.println("Error occurred: " + e.getMessage());
         }
     }
+
+
+    private static HuffmanNode buildHuffmanTree(Map<Character, Integer> frequencyMap) {
+
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+
+            priorityQueue.offer(new HuffmanNode(entry.getKey(), entry.getValue()));
+
+        }
+
+        while (priorityQueue.size() > 1) {
+
+            HuffmanNode left = priorityQueue.poll();
+
+            HuffmanNode right = priorityQueue.poll();
+
+            HuffmanNode parent = new HuffmanNode('\0', left.frequency + right.frequency);
+
+            parent.left = left;
+
+            parent.right = right;
+
+            priorityQueue.offer(parent);
+
+        }
+
+        return priorityQueue.poll();
+
+    }
+
+    private static void generateHuffmanCodes(HuffmanNode root, StringBuilder code) {
+
+        if (root == null)
+            return;
+
+        if (root.left == null && root.right == null) {
+
+            huffmanCodes.put(root.data, code.toString());
+
+            return;
+
+        }
+
+        generateHuffmanCodes(root.left, code.append('0'));
+
+        code.deleteCharAt(code.length() - 1);
+
+        generateHuffmanCodes(root.right, code.append('1'));
+
+        code.deleteCharAt(code.length() - 1);
+
+    }
+
+    private static void encodeFile(FileInputStream inputStream, BitOutputStream bitOutputStream) throws IOException {
+
+        int character;
+
+        while ((character = inputStream.read()) != -1) {
+
+            char c = (char) character;
+
+            String code = huffmanCodes.get(c);
+
+            for (char bit : code.toCharArray()) {
+
+                bitOutputStream.writeBit(bit == '1');
+
+            }
+
+        }
+
+    }
+
 }
--- DecompressFile.java
+++ DecompressFile.java
@@ -1,26 +1,75 @@
 import java.io.*;
+import java.util.*;
 
 public class DecompressFile {
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java DecompressFile <source file> <destination file>");
-            return;
+
+            if (args.length != 2) {
+                System.out.println("Usage: java DecompressFile <source file> <destination file>");
+                return;
+            }
+    
+            String sourceFile = args[0];
+            String destinationFile = args[1];
+    
+            try (FileInputStream inputStream = new FileInputStream(sourceFile);
+                 ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                 FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+    
+                Map<Character, String> huffmanCodes = (Map<Character, String>) objectInputStream.readObject();
+    
+                HuffmanNode root = reconstructHuffmanTree(huffmanCodes);
+    
+                BitInputStream bitInputStream = new BitInputStream(sourceFile);
+    
+                decodeFile(bitInputStream, outputStream, root);
+    
+                bitInputStream.close();
+    
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error occurred: " + e.getMessage());
+            }
         }
 
-        String sourceFile = args[0];
-        String destinationFile = args[1];
+    private static HuffmanNode reconstructHuffmanTree(Map<Character, String> huffmanCodes) {
+        HuffmanNode root = new HuffmanNode('\0', 0);
 
-        try (FileInputStream inputStream = new FileInputStream(sourceFile);
-             FileOutputStream outputStream = new FileOutputStream(destinationFile)) {
+        for (Map.Entry<Character, String> entry : huffmanCodes.entrySet()) {
+            char[] codeArray = entry.getValue().toCharArray();
 
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, length);
+            HuffmanNode current = root;
+
+            for (char bit : codeArray) {
+                if (bit == '0') {
+                    if (current.left == null) {
+                        current.left = new HuffmanNode('\0', 0);
+                    }
+                    current = current.left;
+                } else if (bit == '1') {
+                    if (current.right == null) {
+                        current.right = new HuffmanNode('\0', 0);
+                    }
+                    current = current.right;
+                }
             }
+            current.data = entry.getKey();
+        }
 
-        } catch (IOException e) {
-            System.out.println("Error occurred: " + e.getMessage());
+        return root;
+    }
+
+    private static void decodeFile(BitInputStream bitInputStream, FileOutputStream outputStream, HuffmanNode root) throws IOException {
+        HuffmanNode current = root;
+        int bits;
+        while ((bits = bitInputStream.readBit()) != -1) {
+            for (int i = 7; i >= 0; i--) {
+                int bit = (bits >> i) & 1;
+                current = (bit == 0) ? current.left : current.right;
+                if (current.left == null && current.right == null) {
+                    outputStream.write(current.data);
+                    current = root;
+                }
+            }
         }
     }
 }
